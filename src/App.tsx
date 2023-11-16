1de47eb67e15eb59768a985a920652286da20566
EsaulovaLudmila <esaulova_l_v@mail.ru>
2023-11-17 00:01:45 +0300

    added app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import React from "react";
-
-import "./App.css";
+import styled from "styled-components";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div>
+      <img src="" alt="" />
+      <Title>Headline</Title>
+      <p>
+        Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie
+        ornare in venen.
+      </p>
+      <div>
+        <button>See more</button>
+        <button>See more</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
+
+const Title = styled.h1`
+  font-size: 1.5rem;
+  text-align: center;
+  color: red;
+`;
